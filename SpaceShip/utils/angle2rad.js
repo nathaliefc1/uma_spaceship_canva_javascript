ffc6840f89f42cfb55b518d9abecfa0866ff6f73
nathaliefc1 <nathaliefc89@gmail.com>
2021-05-18 11:59:01 +0200

    Uma spaceship

--- SpaceShip/utils/angle2rad.js
+++
@@ -1,3 +0,0 @@
-const angle2rad = (angle) => (Math.PI / 180) * angle;
-
-export default angle2rad;
--- SpaceShip/utils/checkLimits.js
+++
@@ -1,8 +0,0 @@
-const checkLimits = (pos) => {
-  if (pos.x < 800 && pos.x > 0 && pos.y < 600 && pos.y > 0) {
-    return true;
-  }
-  return false;
-};
-
-export default checkLimits;
